f7e0b4d063e2ddd45dd71935f5ae77ba4c06027d
choeqq <k.chola@op.pl>
2022-11-30 00:36:53 +0100

    list tweets

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,9 +1,52 @@
+import dayjs from "dayjs";
+import Image from "next/image";
+import { RouterOutputs, trpc } from "../utils/trpc";
 import { CreateTweet } from "./CreateTweet";
 
+function Tweet({
+  tweet,
+}: {
+  tweet: RouterOutputs["tweet"]["timeline"]["tweets"][number];
+}) {
+  return (
+    <div className="mb-4 border-b-2 border-gray-500 ">
+      <div className="flex p-2 ">
+        {tweet.author.image && (
+          <Image
+            src={tweet.author.image}
+            alt={`${tweet.author.name} profile picture`}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        )}
+        <div className="ml-2">
+          <div className="align-center flex">
+            <p className="font-bold">{tweet.author.name}</p>
+            <p className="text-sm text-gray-400">
+              {" "}
+              - {new Date(tweet.createdAt).toISOString()}
+            </p>
+          </div>
+
+          <div>{tweet.text}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export function Timeline() {
+  const { data } = trpc.tweet.timeline.useQuery({ limit: 2 });
+
   return (
     <div>
       <CreateTweet />
+      <div className="border-l-2 border-r-2 border-t-2 border-gray-500">
+        {data?.tweets.map((tweet) => (
+          <Tweet key={tweet.id} tweet={tweet} />
+        ))}
+      </div>
     </div>
   );
 }
